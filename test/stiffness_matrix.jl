60cb8c865818734baf77938881c0b284956f7d91
Sean McBane <sean.mcbane@protonmail.com>
2024-02-06 22:15:56 -0600

    Remove unused/unnecessary test code

--- test/stiffness_matrix.jl
+++ test/stiffness_matrix.jl
@@ -5,17 +5,12 @@ using .Stiffness2D: K1_map
 using ForwardDiff
 using ProgressMeter: @showprogress
 
-@testset "2D stiffness tests" begin
-
+@testset "Test the K1 component of the stiffness matrix" begin
     basis_coefficients = coefficients(Float64)
 
     @test size(basis_coefficients) == (16, 16)
 
     naive_basis = [(x, y) -> sum(basis_coefficients[i, j] * terms[j](x, y) for j in 1:16) for i in 1:16]
-
-    let x = Quadrature.five_point_rule_2d(Float64).points[7]
-        @test naive_basis[1](x...) + naive_basis[2](x...) + naive_basis[3](x...) + naive_basis[4](x...) ≈ 1.0
-    end
 
     ∂x(f) = (x, y) -> ForwardDiff.derivative(z -> f(z, y), x)
     ∂y(f) = (x, y) -> ForwardDiff.derivative(z -> f(x, z), y)
@@ -40,36 +35,10 @@ using ProgressMeter: @showprogress
         I
     end
 
-    function naive_K1(lambda)
-        K = Matrix{Float64}(undef, 32, 32)
-        for j = 1:32
-            for i = j:32
-                K[i, j] = K1_element(i, j, lambda)
-            end
-        end
-        Symmetric(K, :L)
-    end
-
-    function reshape_to_symmetric(K::Vector)
-        @assert length(K) == 528
-        dest = Matrix{Float64}(undef, 32, 32)
-        ind = 1
-        for j = 1:32
-            for i = j:32
-                dest[i, j] = K[ind]
-                ind += 1
-            end
-        end
-        Symmetric(dst, :L)
-    end
-
     let M = K1_map(Float64)
         lambda = (x, y) -> 1.0
         L = zeros(16)
         L[1:4] .= 1
-        # Kref = naive_K1(lambda)
-        # K = reshape_to_symmetric(M * L)
-        # @test K ≈ Kref
         i = 1
         j = 1
         K = M * L
